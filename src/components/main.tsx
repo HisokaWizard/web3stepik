f501c4ac57db432b50ec0f38dbe0b12e498ef9e3
Petr Nikolaev <npa040493@gmail.com>
2022-03-25 12:39:16 +0300

    Waves Keeper Auth integrated

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 
 export const Main = () => {
+  const authFunc = React.useCallback(() => {
+    const authData = { data: 'Auth on my site' };
+    if ('WavesKeeper' in window) {
+      (window as any)?.WavesKeeper?.auth(authData)
+        ?.then((auth: any) => {
+          console.log(auth); //displaying the result on the console
+          /*...processing data */
+        })
+        ?.catch((error: string) => {
+          console.error(error); // displaying the result on the console
+          /*...processing errors */
+        });
+    } else {
+      alert('To Auth WavesKeeper should be installed.');
+    }
+  }, []);
+
   return (
     <div className='container'>
-      <input
-        className='btn btn-primary'
-        type='submit'
-        value='Alert'
-        onClick={() => {
-          alert('Alert button onClick');
-        }}
-      />
+      <input className='btn btn-primary' type='submit' value='Alert' onClick={authFunc} />
     </div>
   );
 };
